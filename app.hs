ae6e5db9e05508816e397a30effe9ca062fc7ccc
AnDeacracht <dmaxmueller@gmail.com>
2017-08-24 13:39:28 +0200

    Working things out with the new structure

---
+++ app.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE OverloadedStrings, DeriveGeneric #-}
+
+module App where
+
+import Data.Aeson
+import Data.Text
+import Control.Applicative
+import Control.Monad
+import qualified Data.ByteString.Lazy as B
+import GHC.Generics
+
+
+data Player = Player { 
+	colour :: String,
+	house :: Int, 
+	goal :: Int, 
+	fields :: [String]
+} deriving (Show, Generic)
+
+data GameState = GameState {
+	players :: Player,
+	turn :: String,
+	roll :: Int
+} deriving (Show, Generic)
